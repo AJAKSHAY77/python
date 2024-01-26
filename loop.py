2f00561704af7c89b7b849f4debb330bba6dbf51
akshay jain <ajakshayjain11@gmail.com>
2024-01-26 07:33:41 +0530

    updated

---
+++ loop.py
@@ -0,0 +1,8 @@
+# for i in range(0,11,5):
+#     print(i)
+
+i=0
+while i<5:
+    print(i)
+    i+=1
+    
---
+++ test3.py
@@ -0,0 +1,26 @@
+# num1 = float(input("enter the num1"))
+# num2 = float(input("enter the num2"))
+# num1 = "20"
+# num1 =  int("20")
+
+# opr = input("enter operator")
+# if opr=="/":
+#     output= num1/num2
+# if opr=="%":
+#     output= num1%num2
+# if opr=="*":
+#     output= num1*num2
+
+num1 = int(input("enter the number"))
+
+if num1<3:
+    print("num1 is less then 3")
+
+elif num1==3:
+    print("num1 is equal 3")    
+
+else:
+    print("num1 is greater then 3 ")
+
+# print("your calculation is :",output)
+
