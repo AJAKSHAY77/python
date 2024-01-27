09ae96cdfd4f565b42ecd9bc6148bfe5bd889b31
akshay jain <ajakshayjain11@gmail.com>
2024-01-27 18:38:43 +0530

    updated

---
+++ disc.py
@@ -0,0 +1,10 @@
+my_disc  ={
+    "name":"aksay",
+    "server":"asss",
+    "ram":4545534,
+    "active":True
+}
+if my_disc["active"]:
+# print(my_disc.get("server"))
+# print(my_disc["name"])
+  print(my_disc["ram"])
---
+++ list.py
@@ -0,0 +1,28 @@
+my_list = list(["akshay","aditya"])
+
+print(type(my_list))
+
+my_list = ["ak","ad"]
+print(type(my_list))
+
+
+# for i in my_list:
+#   print("deploying")
+#   my_list.append("akshay jain")
+#   print(i)
+
+# for i in my_list:
+
+# #    print(dir(my_list))
+    
+#  my_list.extend("lappi")
+#  print(my_list)  
+
+new_list  = ["jain","akhsy","jai ho"]
+my_number = list([1,2,3,4,5,56,"akhsy",True,90.0])
+print(my_number)
+
+
+new_list.append("jai ho")
+print(new_list)
+
